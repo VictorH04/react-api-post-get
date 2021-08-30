43a4e92eafa3ffe62e45758966a1e6615b9fe7eb
Victor Hristov <Victor.h7@live.com>
2021-08-30 21:08:15 +0200

    Added logic for submit and changed some minor stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./SASS/base/_Home.scss";
 import { useState, useEffect } from "react";
 import { Route, Switch, useHistory } from "react-router-dom";
+import { format } from "date-fns";
 import Nav from "./components/Nav";
 import Feed from "./components/Feed";
 import Form from "./components/Form";
@@ -10,11 +11,13 @@ import Footer from "./components/Footer";
 import api from "./api/posts";
 
 function App() {
-
   const [posts, setPosts] = useState([]);
+  const [postBody, setPostBody] = useState("");
+  const [postTitle, setPostTitle] = useState("");
+
+  const history = useHistory();
 
   useEffect(() => {
-
     const fetchPosts = async () => {
       try {
         const response = await api.get("/posts");
@@ -33,31 +36,54 @@ function App() {
           console.log(err.message);
         }
       }
-    }
+    };
 
     fetchPosts();
+  }, []);
 
-  }, [])
+  const handleSubmit = async () => {
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const dateTime = format(new Date(), "MMMM dd, yyyy pp");
+    const newPost = { id, title: postTitle, dateTime, body: postBody };
+
+    try {
+      const response = await api.post("/posts", newPost);
+      const allPosts = [...posts, response.data];
+
+      setPosts(allPosts);
+      setPostTitle("");
+      setPostBody("");
+      history.push("/");
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
 
   return (
     <>
       <Nav title="Victor's cool app!" about="About" />
-        <div className="Home">
-          <Switch>
-            <Route exact path="/">
-              <Form />
-              <Feed cards={posts} />
-            </Route>
-            <Route path="/post/:id">
-              <PostPage />
-            </Route>
-            <Route exact path="/about">
-              <About />
-            </Route>
-          </Switch>
-        </div>
+      <div className="Home">
+        <Switch>
+          <Route exact path="/">
+            <Form
+              handleSubmit={handleSubmit}
+              setPostBody={setPostBody}
+              setPostTitle={setPostTitle}
+              postBody={postBody}
+              postTitle={postTitle}
+            />
+            <Feed cards={posts} />
+          </Route>
+          <Route path="/post/:id">
+            <PostPage />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </div>
       <Footer />
-      </>
+    </>
   );
 }
 
